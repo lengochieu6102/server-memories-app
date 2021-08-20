b06e3a64ad925e2fdbeefda173a5c3b055c9eee2
HIẾU Lê Ngọc <hieu.le6102@hcmut.edu.vn>
2021-08-20 17:08:00 +0700

    update login logout authentication

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js ';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
 
 const app = express();
 dotenv.config();
@@ -12,7 +14,10 @@ dotenv.config();
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+app.use('/user',userRoutes);
 app.use('/posts',postRoutes);
+
+
 
 app.get('/',(req, res)=>{
     res.send("hello my app");
